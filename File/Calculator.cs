28dbe1aaf837a9c2f24cf67f5071b312d9446ba7
Aytac <astryou@gmail.com>
2021-03-10 18:04:48 +0100

    Final Test

--- File/Calculator.cs
+++ File/Calculator.cs
@@ -1,16 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace File
 {
     public class Calculator
     {
-        public static double Sum(string str)
+        public static List<char> delimiters = new List<char>() { ',', '\n' };
+        public static double Add(string str)
         {
-            if (str=="") return 0;
-            if (str=="1") return 1;
-            if (str == "2") return 2;
-            if (str == "1,2") return 3;
-            return 4;
+            if (str == "") return 0;
+            var sum = 0;
+            ParseStr(ParseForDelimi(str)).ForEach(s => sum += s);
+            return sum;
+            
         }
+
+        private static List<int> ParseStr(string str)
+        {
+            var values = new List<int>();
+            Array.ForEach(str.Split(delimiters.ToArray()),
+                s => {
+                    var number = int.Parse(s);
+                    if (number < 0) throw new Exception("Negative Value"); 
+                    values.Add(int.Parse(s));
+                });
+            return values;
+        }
+        private static string ParseForDelimi(string str)
+        {
+            if (str.StartsWith("//"))
+            {
+                delimiters.Add(str[2]);
+                str = str.Substring(4);
+            }
+            return str;
+        }
+
     }
 }
--- NUnitTestProject1/CalculatorTest.cs
+++ NUnitTestProject1/CalculatorTest.cs
@@ -1,5 +1,7 @@
 using NUnit.Framework;
 using File;
+using System;
+
 namespace NUnitTestProject1
 {
     public class CalculatorTest
@@ -10,7 +12,43 @@ namespace NUnitTestProject1
             [Values(0, 1, 2, 3)] int expectedValue
             )
         {
-            Assert.AreEqual(Calculator.Sum(x), expectedValue);
+            Assert.AreEqual(Calculator.Add(x), expectedValue);
         }
+
+        [Test, Sequential]
+        public void Multiple(
+            [Values("", "1", "2", "1,2", "1,2,3,4")] string x,
+            [Values(0, 1, 2, 3, 10)] int expectedValue
+            )
+        {
+            Assert.AreEqual(Calculator.Add(x), expectedValue);
+        }
+        [Test, Sequential]
+        public void add_newLine(
+            [Values("", "1", "2", "1\n2,3", "1\n2")] string x,
+            [Values(0, 1, 2, 6, 3)] int expectedValue
+            )
+        {
+            Assert.AreEqual(Calculator.Add(x), expectedValue);
+        }
+
+        [Test, Sequential]
+        public void add_negative(
+          [Values("-1")] string x
+          )
+        {
+            TestDelegate add = () => Calculator.Add(x);
+            Assert.Throws<Exception>(add);
+}
+        [Test, Sequential]
+        public void add_changedeliminator(
+           [Values("", "1", "2", "//;n1;2")] string x,
+           [Values(0, 1, 2, 3)] int expectedValue
+           )
+        {
+            Assert.AreEqual(Calculator.Add(x), expectedValue);
+        }
+
+
     }
 }
