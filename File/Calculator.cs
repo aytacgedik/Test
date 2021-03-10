10dab6aeab386c2946d418625d41f4010ae09525
Aytac <astryou@gmail.com>
2021-03-10 16:26:16 +0100

    Implementaion of CalculatorTest

---
+++ File/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace File
+{
+    public class Calculator
+    {
+        public static double Sum(Double a, Double b)
+        {
+            return a+b;
+        }
+    }
+}
